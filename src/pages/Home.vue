807086973a704c01f4447f3243b3198997534f9d
@Maksim <maxim.shevjakov@inn4science.com>
2019-10-10 19:19:02 +0300

    add number on home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,6 +6,24 @@
       <Map />
       <!-- <img src="../assets/img/shield.png" alt="">
       <p>{{$t('home-text-1')}}</p> -->
+      <div class="project-counter d-flex">
+        <div class="project-col">
+          <h3>8</h3>
+          <p>Country of Operations</p>
+        </div>
+        <div class="project-col">
+          <h3>500</h3>
+          <p>Projects</p>
+        </div>
+        <div class="project-col">
+          <h3>450</h3>
+          <p>Realized Projects</p>
+        </div>
+        <div class="project-col">
+          <h3>12</h3>
+          <p>Creators and Team </p>
+        </div>
+      </div>
     </main>
     <Footer />
   </div>
@@ -43,6 +61,29 @@ export default {
     img{
       width: 100%;
     }
+  }
+  .project-counter{
+    padding-left: 15%;
+    margin-bottom: 120px;
+    margin-top: 40px;
+    .project-col{
+      margin-right: 10%;
+      text-align: left;
+      max-width: 130px;
+      h3{
+        font-size: 64px;
+        line-height: 79px;
+        color: #000000;
+      }
+      p{
+        font-size: 18px;
+        line-height: 22px;
+        text-transform: uppercase;
+        color: #424647;
+      }
+
+    }
+    
   }
 }
 </style>
