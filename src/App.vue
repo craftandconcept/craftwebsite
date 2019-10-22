1eb4edaf159018784bc712b64837e339112cafee
Vladislav Lypivets <avasl333@gmail.com>
2019-10-22 20:03:07 +0300

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@
 </template>
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
+import '@/assets/fonts/fonts.css'
 export default {
   name: 'App'
 }
 </script>
 <style lang="scss">
+body{
+  font-family: 'CoreSans';
+}
 button:focus,
 button:active{
   outline: none;
