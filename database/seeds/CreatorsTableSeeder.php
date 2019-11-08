c83dc12a5d74f7792acab95b61b353ba15a0df9f
intent15 <dfalcon.7c8@gmail.com>
2019-11-08 16:59:35 +0200

    update creators seed

--- database/seeds/CreatorsTableSeeder.php
+++ database/seeds/CreatorsTableSeeder.php
@@ -11,17 +11,49 @@ class CreatorsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
         DB::table('creators')->insert([
-            'creator_name' => 'Author 1',
+            'creator_name' => 'Oleg Drozdov (DP)',
 
         ]);
         DB::table('creators')->insert([
-            'creator_name' => 'Author 2',
+            'creator_name' => 'Tymofii Ulanchenko (DP)',
 
         ]);
         DB::table('creators')->insert([
-            'creator_name' => 'Author 3',
+            'creator_name' => 'Vyacheslav Zhemir',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'AIM Group, Ivan Malinka and Abueva Anastasiya',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Yana and Andrii Briukhovetskiy',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Sergii and Igor Fomichov',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Ilya and Mariia Pavlovy',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Alexander Dizzroom',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Pavel Mito',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Evgeniy',
+
+        ]);
+        DB::table('creators')->insert([
+            'creator_name' => 'Vlad Vishnevskiy',
 
         ]);
     }
