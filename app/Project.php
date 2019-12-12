6bd46fdac7e07450206bd3e4f0eb7b4fdd5345d9
Dmitriy Sokol <dfalcon.7c8@gmail.com>
2019-12-12 13:30:39 +0200

    1

--- app/Project.php
+++ app/Project.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
-     protected $fillable = ['name','country_id','collaborators','size','main_description', 'city', 'status','function','photos_by','main_image', 'main_text', 'creator_id'];
+     protected $fillable = ['name','country_id','collaborators','size','main_description', 'city', 'status','function','photos_by','main_image', 'main_text', 'creator'];
 
     public function categories(){
         return $this->belongsToMany('App\Category');
