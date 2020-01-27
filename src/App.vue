6a4f7daa4ef9ab1b4427b6d9ef39cb0fb213ecd8
Vladislav Lypivets <avasl333@gmail.com>
2020-01-27 20:46:53 +0200

    reverse projects and fix triangle

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script>
-import '@/assets/fonts/fonts.css'
+// import '@/assets/fonts/fonts.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 export default {
